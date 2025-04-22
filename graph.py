790c4c32cce0d4f4fa671f04f4ae3b8db3cf5632
sujanis <sujani.srini@gmail.com>
2025-04-22 01:06:08 -0500

    work

--- graph.py
+++ graph.py
@@ -68,10 +68,6 @@ class Node:
         """
         self.data = data
         self.next = next
-    def get_data(self):
-        return self.data
-    def get_next(self):
-        return self.next
 
 
 class StackError(Exception):
